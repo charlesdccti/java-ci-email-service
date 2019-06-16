4592ada8ee9ab0ea81fef58a69b69398edafafe0
Marylly Araújo Silva <mymarylly@gmail.com>
2019-06-16 14:35:16 -0300

    Remove unused imports and include controller dependency.

--- src/main/java/com/emailService/emailService/EmailServiceApplication.java
+++ src/main/java/com/emailService/emailService/EmailServiceApplication.java
@@ -1,24 +1,16 @@
 package com.emailService.emailService;
 
+import com.emailService.controller.EmailController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.emailService.consumer.Consumer;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = { Consumer.class })
+@ComponentScan(basePackageClasses = { EmailController.class })
 @EnableAutoConfiguration
 public class EmailServiceApplication {
-
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
