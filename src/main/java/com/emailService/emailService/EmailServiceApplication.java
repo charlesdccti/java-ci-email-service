995b1033496c7deba6cc371d723921fce0487d82
Marylly Araújo Silva <mymarylly@gmail.com>
2019-06-16 13:48:31 -0300

    Fix imports of EmailServiceApp

--- src/main/java/com/emailService/emailService/EmailServiceApplication.java
+++ src/main/java/com/emailService/emailService/EmailServiceApplication.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.emailService.consumer.Consumer;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = { Consumer.class })
+@EnableAutoConfiguration
 public class EmailServiceApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
