206e6f585ed78471437a271791ceab10934b695c
Marylly Araújo Silva <mymarylly@gmail.com>
2019-08-02 19:35:53 -0300

    Include PMD, Findbugs and Jacoco in the circleci pipeline

--- src/main/java/com/emailService/consumer/Consumer.java
+++ src/main/java/com/emailService/consumer/Consumer.java
@@ -8,8 +8,11 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 @Component
 public class Consumer {
 
+	private String bar = new String("bar");
+
 	@JmsListener(destination = "filaPedidoEmail")
-	public void receiveMessage(String message) {
-	    System.out.println(message);
+	public void receiveMessage(String message){
+		System.out.println(message);
+		// System.exit(0);
 	}
 }
